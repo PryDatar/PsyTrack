5b73290c26ac07fd6b1c41f26283f21bfb870c60
PryDatar <pierrick.delort@epita.fr>
2023-07-06 15:03:28 +0200

    [ADD] Init repo

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>PsyTrack</h1>
+      <p>Bienvenue sur la page d'accueil !</p>
+    </div>
+  );
+};
+
+export default HomePage;
